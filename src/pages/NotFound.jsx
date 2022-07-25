32e9bf25476e0ca49898fff4d0ea317caa0bdb4f
Emilce Daiana Molina <emmolina@emeal.nttdata.com>
2022-07-25 19:55:38 -0300

    mje de error

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import ThumbDownAltIcon from '@mui/icons-material/ThumbDownAlt';
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className='no-encontrado'>
+      page not found
+      <ThumbDownAltIcon  style={{color:"white"}}/>
+      </div>
   )
 }
 
